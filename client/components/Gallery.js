763beaa89c28b8732d9963c4729535416469636e
Brittany Fenney <brittanyfenney@gmail.com>
2021-03-24 15:42:33 -0700

    add home and routes

---
+++ client/components/Gallery.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Gallery() {
+  return (
+    <div id="gallery-outer">
+      <div id="gallery">
+        <h1>Gallery</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Gallery
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,24 +6,50 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
+    <div id="nav-container">
+      <img
+        src="https://i.pinimg.com/originals/c9/f1/58/c9f1585926ebc34913646266212c1310.png"
+        id="logo"
+      />
+      <h1 id="title">COLLABALLAGE</h1>
+      <nav>
+        {isLoggedIn ? (
+          <div>
+            {/* The navbar will show these links after you log in */}
+            <Link to="/home">Home</Link>
+            <a href="#" onClick={handleClick}>
+              Logout
+            </a>
+          </div>
+        ) : (
+          <div>
+            {/* The navbar will show these links before you log in */}
+            <Link to="/">
+              {/* <button type="button" className="nav-button">Home</button> */}
+              <div className="nav-button">
+                <h3>Home</h3>
+              </div>
+            </Link>
+            <Link to="/auth">
+              <div className="nav-button">
+                <h3>Login/Signup</h3>
+              </div>
+            </Link>
+
+            <Link to="/join">
+              <div className="nav-button">
+                <h3>Create/Join a Room</h3>
+              </div>
+            </Link>
+            <Link to="/gallery">
+              <div className="nav-button">
+                <h3>Gallery</h3>
+              </div>
+            </Link>
+          </div>
+        )}
+      </nav>
+    </div>
     <hr />
   </div>
 )
