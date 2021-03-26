7833c6a444170b1a242d6f0e597687db26d42c5d
Brittany Fenney <brittanyfenney@gmail.com>
2021-03-26 08:31:34 -0700

    add additional img filters

---
+++ client/store/chat.js
@@ -0,0 +1,48 @@
+import realtimeDB from '../../server/db/firebase'
+
+//action types
+const STORE_MESSAGE = 'STORE_MESSAGE'
+
+//action creators
+const storeMessage = message => ({
+  type: STORE_MESSAGE,
+  message
+})
+
+//thunks
+export const getImages = () => async dispatch => {
+  try {
+    let images = []
+    await firestore
+      .collection('images')
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          images.push({
+            url: doc.data().url,
+            tags: doc.data().tags
+          })
+        })
+      })
+      .catch(error => {
+        console.log('Error getting documents: ', error)
+      })
+    const imagesCopy = JSON.parse(JSON.stringify(images))
+    console.log(imagesCopy)
+    dispatch(gotImages(imagesCopy))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const initialState = []
+
+//reducer
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GOT_IMAGES:
+      return action.images
+    default:
+      return state
+  }
+}
