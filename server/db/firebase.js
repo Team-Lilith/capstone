ebf2e2f5d6b6c1fe7e0e797fda59a10b60bc8e31
Brittany Fenney <brittanyfenney@gmail.com>
2021-03-31 08:25:02 -0700

    test heroku auth

--- server/db/firebase.js
+++ server/db/firebase.js
@@ -3,23 +3,23 @@ require('firebase/auth')
 require('firebase/firestore')
 require('firebase/database')
 
-// var FIREBASE_CONFIG = require('./fbconfig.json')
+var FIREBASE_CONFIG = require('./fbconfig.json')
 
-let firebaseConfig = {
-  apiKey: 'AIzaSyCpJMMeWhHixunuu6h_5BF7Xo7V6z1AnRI',
-  authDomain: 'capstone-e6ea2.firebaseapp.com',
-  databaseURL: 'https://capstone-e6ea2-default-rtdb.firebaseio.com',
-  projectId: 'capstone-e6ea2',
-  storageBucket: 'capstone-e6ea2.appspot.com',
-  messagingSenderId: '133828787014',
-  appId: '1:133828787014:web:ad8b29f5afdde84e074347'
-}
-
-// if (process.env.NODE_ENV === 'development') {
-//   firebaseConfig = FIREBASE_CONFIG
-// } else {
-//   firebaseConfig = process.env.FIREBASE_CONFIG
+// let firebaseConfig = {
+//   apiKey: 'AIzaSyCpJMMeWhHixunuu6h_5BF7Xo7V6z1AnRI',
+//   authDomain: 'capstone-e6ea2.firebaseapp.com',
+//   databaseURL: 'https://capstone-e6ea2-default-rtdb.firebaseio.com',
+//   projectId: 'capstone-e6ea2',
+//   storageBucket: 'capstone-e6ea2.appspot.com',
+//   messagingSenderId: '133828787014',
+//   appId: '1:133828787014:web:ad8b29f5afdde84e074347'
 // }
+
+if (process.env.NODE_ENV === 'development') {
+  firebaseConfig = FIREBASE_CONFIG
+} else {
+  firebaseConfig = process.env.FIREBASE_CONFIG
+}
 
 // Your web app's Firebase configuration
 // if (process.env.NODE_ENV === 'development') {
