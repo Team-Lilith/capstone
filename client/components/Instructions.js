fe16631eea7aa3f2ba11e074b20605982e097308
Jamie Eunice <jamieecarrasquillo@gmail.com>
2021-03-31 23:50:35 -0500

    feat(instructions) style instructions component

--- client/components/Instructions.js
+++ client/components/Instructions.js
@@ -1,25 +1,24 @@
 import React from 'react'
+import '../index.css'
 
 function Instructions(props) {
   return (
-    <div id="join-outer">
-      <div className="join">
-        <h3>Room Guide</h3>
-        <div>
-          <p>
-            🎨Create a room by entering a room name of preference or just
-            clicking the `start a new room` button
-          </p>
-          <p>
-            🎨If a friend already created a room, enter the room name and click
-            the `join an existing room` button
-          </p>
-          <p>
-            🎨If that seems like too much hassle, just create a room and share
-            your link
-          </p>
-          <p>Note: Each room has a limit of two users per session.</p>
-        </div>
+    <div id="instructions">
+      <h2>Room Guide</h2>
+      <div id="instructions-inner">
+        <p>
+          🎨Create a room by entering a room name of preference or just clicking
+          the `start a new room` button
+        </p>
+        <p>
+          🎨If a friend already created a room, enter the room name and click
+          the `join an existing room` button
+        </p>
+        <p>
+          🎨If that seems like too much hassle, just create a room and share
+          your link
+        </p>
+        <p>Note: Each room has a limit of two users per session.</p>
       </div>
     </div>
   )
